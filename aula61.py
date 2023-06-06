f21e2ba0231069fa3dec4f0dbc878793eb9b3ee3
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2023-06-06 16:26:59 -0300

    Até aula 101

--- aula61.py
+++ aula61.py
@@ -20,3 +20,22 @@ contrário disso:
     resultado é o valor da conta
 O primeiro dígito do CPF é 7
 """
+
+# cpf = '74682489070'
+cpf = '37696055003'
+nove_digitos = cpf[:9]
+print('Os primeiros 9 dígitos do CPF são:', nove_digitos)
+
+resultado_digito_1 = 0
+contador_regressivo_1 = 10
+
+for digito_1 in nove_digitos:
+    print(int(digito_1), contador_regressivo_1)
+    resultado_digito_1 += int(digito_1) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+
+digito_1  = (resultado_digito_1 * 10) % 11
+
+digito_1 = digito_1 if digito_1 <=9 else 0
+
+print(digito_1)

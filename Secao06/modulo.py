5d13fa0ce0a6daceb275abd1ce55dcf460fc025f
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2023-12-06 10:58:30 -0300

    Aula 273 - função Main

---
+++ Secao06/modulo.py
@@ -0,0 +1,12 @@
+def soma(x: float, y: float) -> float:
+    return x + y
+
+
+def main() -> None:
+    print(soma(10, 20))
+    print(soma(20, 30))
+
+
+if __name__ == '__main__':
+    print(__name__)
+    main()

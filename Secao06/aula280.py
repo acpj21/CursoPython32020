8d5d869525083037a8337143f1c4e085eaa7a639
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2023-12-08 15:21:53 -0300

    Locale para internacionalização

---
+++ Secao06/aula280.py
@@ -0,0 +1,13 @@
+# locale para internacionalização (tradução)
+# https://docs.python.org/3/library/locale.html
+# https://learn.microsoft.com/fr-fr/powershell/module/international/get-winsystemlocale?view=windowsserver2022-ps&viewFallbackFrom=win10-ps
+
+import calendar
+
+print(calendar.calendar(2022))
+
+
+import locale
+locale.setlocale(locale.LC_ALL, '')
+
+print(calendar.calendar(2022))

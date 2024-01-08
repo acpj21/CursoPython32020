b9b455c14ead9f05d03a3c2d60be72cfdf13ed14
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2024-01-08 14:55:17 -0300

    Web Scraping cin Python, parte 2.

--- Secao06/aula312.py
+++ Secao06/aula312.py
@@ -8,6 +8,7 @@
 # + Instalação
 # - pip install requests types-requests bs4
 
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -20,5 +21,16 @@ raw_html = response.text
 parsed_html = BeautifulSoup(raw_html, 'html.parser')
 
 # print(parsed_html)
-if parsed_html.title is not None:
-    print(parsed_html.title.text)
+# if parsed_html.title is not None:
+#     print(parsed_html.title.text)
+
+top_jobs_heading = parsed_html.select_one('#intro > div > div > article > h2')
+
+if top_jobs_heading is not None:
+    # print(top_jobs_heading.text)
+    article = top_jobs_heading.parent
+
+    if article is not None:
+        # print(article)
+        for p in article.select('p'):
+            print(re.sub(r'\s{1,} ', ' ', p.text).strip())

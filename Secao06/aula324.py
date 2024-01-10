ad7bfb60884baf5d1e4cb0bbfcc0f7fdf2f4567f
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2024-01-10 16:14:01 -0300

    Theads - Executando processamentos em paralelo

--- Secao06/aula324.py
+++ Secao06/aula324.py
@@ -1,8 +1,35 @@
+# from time import sleep
+# print('Hello')
+
+# for i in range(10):
+#     print(i)
+#     sleep(.5)
+
+# print('World!')
+
 from time import sleep
-print('Hello')
+from threading import Thread
 
-for i in range(10):
+
+class MeuThead(Thread):
+    def __init__(self, texto, tempo):
+        self.texto = texto
+        self.tempo = tempo
+
+        super().__init__()
+   
+    def run(self):
+        sleep(self.tempo)
+        print(self.texto)
+
+
+t1 = MeuThead('Thread 1', 5)
+t1.start()
+
+t2 = MeuThead('Thread 2', 2)
+t2.start()
+
+
+for i in range(20):
     print(i)
-    sleep(.5)
-
-print('World!')
+    sleep(1)

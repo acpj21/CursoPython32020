d696bab42bc8d8f43774fef48032d018eb15b7b1
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2024-01-10 16:24:09 -0300

    Theads - Executando processamentos em paralelo. PArte 2

--- Secao06/aula324.py
+++ Secao06/aula324.py
@@ -7,29 +7,70 @@
 
 # print('World!')
 
+# from time import sleep
+# from threading import Thread
+
+
+# class MeuThead(Thread):
+#     def __init__(self, texto, tempo):
+#         self.texto = texto
+#         self.tempo = tempo
+
+#         super().__init__()
+   
+#     def run(self):
+#         sleep(self.tempo)
+#         print(self.texto)
+
+
+# t1 = MeuThead('Thread 1', 5)
+# t1.start()
+
+# t2 = MeuThead('Thread 2', 2)
+# t2.start()
+
+
+# for i in range(20):
+#     print(i)
+#     sleep(1)
+
+# from time import sleep
+# from threading import Thread
+
+
+# def vai_demorar(texto, tempo):
+#     sleep(tempo)
+#     print(texto)
+
+
+# t1 = Thread(target=vai_demorar, args=('Olá mundo 1!', 5))
+# t1.start()
+
+# t2 = Thread(target=vai_demorar, args=('Olá mundo 2!', 1))
+# t2.start()
+
+# t3 = Thread(target=vai_demorar, args=('Olá mundo 3!', 2))
+# t3.start()
+
+# for i in range(20):
+#     print(i)
+#     sleep(.5)
+
 from time import sleep
 from threading import Thread
 
 
-class MeuThead(Thread):
-    def __init__(self, texto, tempo):
-        self.texto = texto
-        self.tempo = tempo
-
-        super().__init__()
-   
-    def run(self):
-        sleep(self.tempo)
-        print(self.texto)
+def vai_demorar(texto, tempo):
+    sleep(tempo)
+    print(texto)
 
 
-t1 = MeuThead('Thread 1', 5)
+t1 = Thread(target=vai_demorar, args=('Olá mundo 1!', 10))
 t1.start()
+t1.join()
 
-t2 = MeuThead('Thread 2', 2)
-t2.start()
+# while t1.is_alive():
+#     print('Esperando a thead.')
+#     sleep(2)
 
-
-for i in range(20):
-    print(i)
-    sleep(1)
+print('Thread acabou!')

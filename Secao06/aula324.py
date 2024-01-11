07090fa61bf990c91b812c9b018b379f33e48314
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2024-01-11 14:17:44 -0300

    Theads - Executando processamentos em paralelo. Parte 3

--- Secao06/aula324.py
+++ Secao06/aula324.py
@@ -56,21 +56,75 @@
 #     print(i)
 #     sleep(.5)
 
+# from time import sleep
+# from threading import Thread
+
+
+# def vai_demorar(texto, tempo):
+#     sleep(tempo)
+#     print(texto)
+
+
+# t1 = Thread(target=vai_demorar, args=('Olá mundo 1!', 10))
+# t1.start()
+# t1.join()
+
+# # while t1.is_alive():
+# #     print('Esperando a thead.')
+# #     sleep(2)
+
+# print('Thread acabou!')
+
+from threading import Lock, Thread
 from time import sleep
-from threading import Thread
 
 
-def vai_demorar(texto, tempo):
-    sleep(tempo)
-    print(texto)
+class Ingressos:
+    """
+    Classe que vende ingressos
+    """
 
+    def __init__(self, estoque: int):
+        """ Inicializando...
 
-t1 = Thread(target=vai_demorar, args=('Olá mundo 1!', 10))
-t1.start()
-t1.join()
+        :param estoque: quantidade de ingressos em estoque
+        """
+        self.estoque = estoque
+        # Nosso cadeado
+        self.lock = Lock()
 
-# while t1.is_alive():
-#     print('Esperando a thead.')
-#     sleep(2)
+    def comprar(self, quantidade: int):
+        """
+        Compra determinada quantidade de ingressos
 
-print('Thread acabou!')
+        :param quantidade: A quantidade de ingressos que deseja comprar
+        :type quantidade: int
+        :return: Nada
+        :rtype: None
+        """
+        # Tranca o método
+        self.lock.acquire()
+
+        if self.estoque < quantidade:
+            print('Não temos ingressos suficientes.')
+            # Libera o método
+            self.lock.release()
+            return
+
+        sleep(1)
+
+        self.estoque -= quantidade
+        print(f'Você comprou {quantidade} ingresso(s). '
+              f'Ainda temos {self.estoque} em estoque.')
+
+        # Libera o método
+        self.lock.release()
+
+if __name__ == '__main__':
+    ingressos = Ingressos(10)
+
+    for i in range(1, 20):
+        t = Thread(target=ingressos.comprar, args=(i,))
+        t.start()
+
+    print(ingressos.estoque)

1946b2e8f3f24bfeb14fab0093e2917480ed2728
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2024-01-17 16:00:09 -0300

    Manipulando as planilhas do Workbook

--- Secao06/aula333/main.py
+++ Secao06/aula333/main.py
@@ -21,7 +21,20 @@ WORKBOOK_PATH = ROOT_FOLDER / 'workbook.xlsx'
 workbook = Workbook()
 
 # Uma planilha específica
-worksheet: Worksheet = workbook.active
+# worksheet: Worksheet = workbook.active
+
+# Nome para a Planilha
+sheet_name = 'Minha planilha'
+# Criamos a planilha
+workbook.create_sheet(sheet_name, 0)
+
+# Selecionamos a planilha
+# worksheet: Worksheet = workbook.active
+worksheet: Worksheet = workbook[sheet_name]
+# print(workbook.sheetnames)
+
+# Remover uma planilha
+workbook.remove(workbook['Sheet'])
 
 # Criando os cabeçalhos
 worksheet.cell(1, 1, 'Nome')

37d9f1ea08d7ff9d147ccdc14269f1f9cac93939
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2024-01-31 10:47:38 -0300

    Calculadora: botões especiais de operadores, clear e equation

--- Secao07/aula339-calculadora/buttons.py
+++ Secao07/aula339-calculadora/buttons.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from display import Display
     from info import Info
-    
+
 
 class Button(QPushButton):
     def __init__(self, *args, **kwargs):
@@ -37,6 +37,12 @@ class ButtonsGrid(QGridLayout):
         self.display = display
         self.info = info
         self._equation = ''
+        self._equationInitialValue = 'Sua conta'
+        self._left = None
+        self._rignt = None
+        self._op = None
+
+        self.equation = self._equationInitialValue
         self._makeGrid()
 
     @property
@@ -69,6 +75,12 @@ class ButtonsGrid(QGridLayout):
         
         if text == 'C':
             self._connectButtonClicked(button, self._clear)
+        
+        if text in '+-/*':
+            self._connectButtonClicked(
+                button, 
+                self._makeSlot(self._operatorClicked, button)
+                )
     
     def _makeSlot(self, func, *args, **kwargs):
         @Slot(bool)
@@ -86,5 +98,27 @@ class ButtonsGrid(QGridLayout):
         self.display.insert(buttonText)
 
     def _clear(self):
-        print('Vou fazer outra coisa aqui')
+        self._left = None
+        self._rignt = None
+        self._op = None
+        self.equation = self._equationInitialValue
         self.display.clear()
+
+    def _operatorClicked(self, button):
+        buttonText = button.text()  # +-/* (etc ...)
+        displayText = self.display.text()  # Deverá ser meu número _left
+        self.display.clear()  # Limpa o display
+
+        # Se a pessoa clicou no operador sem
+        # configurar qualquer número
+        if not isValidNumber(displayText) and self._left is None:
+            print('Não tem nada para colocar no valor da esquerda.')
+            return
+
+        # Se houver algo no número da esquerda,
+        # não fazemos nada. Aguardaremos o número da direita.
+        if self._left is None:
+            self._left = float(displayText)
+        
+        self._op = buttonText
+        self.equation = f'{self._left} {self._op} ??'

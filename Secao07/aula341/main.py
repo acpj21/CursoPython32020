0e8f72352da4e0a372ab3d1804804fd9dab9024d
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2024-02-15 10:45:06 -0300

    OObject e QTherad: tela travada devido a processamento demorado

--- Secao07/aula341/main.py
+++ Secao07/aula341/main.py
@@ -1,12 +1,25 @@
+import sys
+import time
+
 from PySide6.QtWidgets import QApplication, QWidget
 from ui_workerui import Ui_myWidget
-import sys
 
 
 class MyWidget(QWidget, Ui_myWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+
+        self.Button1.clicked.connect(self.hardWork1)
+        self.Button2.clicked.connect(self.hardWork2)
+    
+    def hardWork1(self):
+        time.sleep(5)
+        self.label1.setText('hardWorkd1 terminado')
+    
+    def hardWork2(self):
+        time.sleep(5)
+        self.label2.setText('2 terminado')
 
 
 if __name__ == '__main__':

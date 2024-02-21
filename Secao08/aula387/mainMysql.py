7137bf2db080a6abe9dba439d6f020d4f20946c2
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2024-02-21 11:30:04 -0300

    CREATE TABLE para criar tabela com PRIMARY KEY no PyMySQL

--- Secao08/aula387/mainMysql.py
+++ Secao08/aula387/mainMysql.py
@@ -7,27 +7,47 @@ import os
 import dotenv
 import pymysql
 
+TABLE_NAME = 'customers'
+
 dotenv.load_dotenv()
 
 connection = pymysql.connect(
-    # host='localhost',
-    # user='usuario',
-    # password='senha',
-    # database='base_de_dados',
     host=os.environ['MYSQL_HOST'],
     user=os.environ['MYSQL_USER'],
     password=os.environ['MYSQL_PASSWORD'],
     database=os.environ['MYSQL_DATABASE'],
+    charset='utf8mb4'
 )
 
 with connection:
     with connection.cursor() as cursor:
         # SQL
         cursor.execute(  # type: ignore
-            'CREATE TABLE IF NOT EXISTS customers ('
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
             'id INT NOT NULL AUTO_INCREMENT, '
             'nome VARCHAR(50) NOT NULL, '
             'idade INT NOT NULL, '
             'PRIMARY KEY (id)'
             ') '
         )
+        # CUIDADO: ISSO LIMPA A TABELA
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')  # type: ignore
+    connection.commit()
+
+    # Começo a manipular dados a partir daqui
+
+    with connection.cursor() as cursor:
+        cursor.execute(  # type: ignore
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUES ("Luiz", 25) '
+        )
+        cursor.execute(  # type: ignore
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUES ("Luiz", 25) '
+        )
+        result = cursor.execute(  # type: ignore
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUES ("Luiz", 25) '
+        )
+        print(result)
+    connection.commit()

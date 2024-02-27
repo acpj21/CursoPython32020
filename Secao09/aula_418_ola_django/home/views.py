ffe1549797d007289aa36f824adb2c8711957295
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2024-02-27 11:59:35 -0300

    Renderizando HTML, render, templates, INSTALLED+APPS e TemplateDoesNotExist e ignorando arquivos de cache

--- Secao09/aula_418_ola_django/home/views.py
+++ Secao09/aula_418_ola_django/home/views.py
@@ -1,9 +1,9 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render
 
 
 def home(request):
     print('HOME')
-    return HttpResponse('home do app 1')
+    return render(
+        request,
+        'home/index.html'
+    )

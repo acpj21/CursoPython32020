adfb1fa0264cc576251237313ab0b1feb6a35abe
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2024-02-28 10:43:10 -0300

    Usando o context para enviar dados para dentro dos templates do Django

--- Secao09/aula_418_ola_django/home/views.py
+++ Secao09/aula_418_ola_django/home/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 
 def home(request):
     print('HOME')
+
+    context = {
+            'text': 'Olá home'
+    }
+
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context
     )
